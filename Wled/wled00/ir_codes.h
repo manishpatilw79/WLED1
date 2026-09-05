5dd10c5c7e3bd6995cf47fdf4a7f897d32d0940c
manishpatilw79 <manishpatilw79@gmail.com>
2026-09-05 14:27:35 +0530

    Update ir_codes.h

--- Wled/wled00/ir_codes.h
+++ Wled/wled00/ir_codes.h
@@ -144,29 +144,29 @@
 #define IR40_SLOW          0xFFD02F  // SLOW
 
 // 44-key defs
-#define IR44_BPLUS         0xFF3AC5  //
-#define IR44_BMINUS        0xFFBA45  //
+#define IR44_BPLUS         0x1FEA05F  //
+#define IR44_BMINUS        0x1FE609F  //
 #define IR44_OFF           0xFF827D  //
-#define IR44_ON            0xFF02FD  //
-#define IR44_RED           0xFF1AE5  //
-#define IR44_GREEN         0xFF9A65  //
-#define IR44_BLUE          0xFFA25D  //
-#define IR44_WHITE         0xFF22DD  // natural white
+#define IR44_ON            0x1FE48B7  //
+#define IR44_RED           0x1FE50AF  //
+#define IR44_GREEN         0x1FED827  //
+#define IR44_BLUE          0x1FEF807  //
+#define IR44_WHITE         0x1FEE01F  // natural white
 #define IR44_REDDISH       0xFF2AD5  //
 #define IR44_GREENISH      0xFFAA55  //
 #define IR44_DEEPBLUE      0xFF926D  //
 #define IR44_WARMWHITE2    0xFF12ED  // warmest white
-#define IR44_ORANGE        0xFF0AF5  //
+#define IR44_ORANGE        0x1FE708F  //
 #define IR44_TURQUOISE     0xFF8A75  //
-#define IR44_PURPLE        0xFFB24D  //
+#define IR44_PURPLE        0x1FEF00F  //
 #define IR44_WARMWHITE     0xFF32CD  // warm white
 #define IR44_YELLOWISH     0xFF38C7  //
-#define IR44_CYAN          0xFFB847  //
+#define IR44_CYAN          0x1FEB04F  //
 #define IR44_MAGENTA       0xFF7887  //
 #define IR44_COLDWHITE     0xFFF807  // cold white
-#define IR44_YELLOW        0xFF18E7  //
+#define IR44_YELLOW        0x1FE30CF  //
 #define IR44_AQUA          0xFF9867  //
-#define IR44_PINK          0xFF58A7  //
+#define IR44_PINK          0x1FE9867  //
 #define IR44_COLDWHITE2    0xFFD827  // coldest white
 #define IR44_REDPLUS       0xFF28D7  //
 #define IR44_GREENPLUS     0xFFA857  //
@@ -188,6 +188,7 @@
 #define IR44_JUMP7         0xFFA05F  //
 #define IR44_FADE3         0xFF609F  //
 #define IR44_FADE7         0xFFE01F  //
+
 
 //Infrared codes for 21-key remote https://images-na.ssl-images-amazon.com/images/I/51NMA0XucnL.jpg
 #define IR21_BRIGHTER      0xFFE01F
